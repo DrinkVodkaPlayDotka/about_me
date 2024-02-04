d111c6ec60799c8e271ddee5d8feada4ec8627e4
Stepanov Mikhail <volodya-stepanov-68@mail.ru>
2024-02-04 17:24:15 +0300

    feat(style): add background

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style >
+@import "./styles/global-styles.css";
+
+</style>
---
+++ src/views/mainContainer.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import BackgroundComponent from "@/components/backgroundComponent.vue";
+
+export default defineComponent({
+  components: {
+    BackgroundComponent
+
+  }
+})
+</script>
+
+<template>
+  <background-component />
+</template>
+
+<style scoped></style>
